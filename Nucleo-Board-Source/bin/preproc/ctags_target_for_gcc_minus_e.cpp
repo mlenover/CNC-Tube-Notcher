bef88ae349bdb1297d499b555d817344c5fa2b7d
mlenover <mlenover@gmail.com>
2019-12-02 22:42:39 -0500

    Added some acceleration functions

--- Nucleo-Board-Source/bin/preproc/ctags_target_for_gcc_minus_e.cpp
+++ Nucleo-Board-Source/bin/preproc/ctags_target_for_gcc_minus_e.cpp
@@ -1,20 +1,74 @@
 # 1 "c:\\Users\\MLeno\\Documents\\GitHub\\CNC-Tube-Notcher\\Nucleo-Board-Source\\main.h"
 # 1 "c:\\Users\\MLeno\\Documents\\GitHub\\CNC-Tube-Notcher\\Nucleo-Board-Source\\main.ino"
-# 2 "c:\\Users\\MLeno\\Documents\\GitHub\\CNC-Tube-Notcher\\Nucleo-Board-Source\\main.ino" 2
+
+
+
+
+//FOR TESTINGS VALUES
+float r1 = 0;
+float r2 = 3;
+float theta1 = 0;
+float theta2 = 0;
+float z1 = 0;
+float z2 = 2;
+float feedrate = 100;
+//END OF TESTING VALUES
 
 int i;
-// the setup function runs once when you press reset or power the board
-void setup() {
-  // initialize digital pin LED_BUILTIN as an output.
-  pinMode(13, 0x1);
+
+float deltaz = z2 - z1;
+float deltar = r2 - r1;
+float deltatheta = theta2 - theta1;
+
+float delta[3] = {deltar, deltatheta, deltaz};
+
+float r_speed;
+float theta_speed;
+float z_speed;
+
+float speed[3];
+
+float steps_per[3] {100, 10 /*Max acceleration of rotation axis in rad/s^2*/, 10 /*Max acceleration of z axis in mm/s^2*/};
+
+float getNextCommand(){
+
 }
 
-// the loop function runs over and over again forever
-void loop() {
-  digitalWrite(13, 0x1); // turn the LED on (HIGH is the voltage level)
-  i = 1;
-  delay(1000); // wait for a second
-  digitalWrite(13, 0x0); // turn the LED off by making the voltage LOW
-  i = 0;
-  delay(1000); // wait for a second
+float *getSpeed(float feedrate, float delta[3], float speed[3]) //Takes feedrate & array of r, theta, z, delta values
+                                                                    //Returns array of speeds for r, theta, z
+{
+    deltar = delta[1];
+    deltatheta = delta[2];
+    deltaz = delta[3];
+
+    float r_avg = (r2+r1)/2;
+    if (deltar != 0) {
+        r_speed = feedrate / sqrt(((deltaz/deltar)*(deltaz/deltar)) + ((r_avg)*(r_avg))*((deltatheta/deltar)*(deltatheta/deltar)) + 1);
+        theta_speed = (deltatheta / deltar)*r_speed;
+        z_speed = (deltaz / deltar)*r_speed;
+    }
+    else if (deltatheta != 0){
+        r_speed = 0;
+        theta_speed = feedrate / sqrt(((r_avg)*(r_avg)) + ((deltaz/deltatheta)*(deltaz/deltatheta)));
+        z_speed = (deltaz / deltatheta) * theta_speed;
+    }
+    else {
+        r_speed = 0;
+        theta_speed = 0;
+        z_speed = feedrate;
+    }
+    speed[1] = abs(r_speed);
+    speed[2] = abs(theta_speed);
+    speed[3] = abs(z_speed);
+    return speed;
+}
+
+void setup()
+{
+    Serial2.begin(9600);
+}
+
+void loop()
+{
+    int i = 1;
 }
--- Nucleo-Board-Source/main.h
+++ Nucleo-Board-Source/main.h
@@ -1,3 +1,13 @@
 #ifndef _INCL_MAIN
 #define _INCL_MAIN
 #endif
+
+#define R_STEPS_PER_MM 100
+#define THETA_STEPS_PER_RAD 1000
+#define Z_STEPS_PER_MM 100
+
+#define R_MAX_ACCEL 10 //Max acceleration of plunge axis in mm/s^2
+#define THETA_MAX_ACCEL 10 //Max acceleration of rotation axis in rad/s^2
+#define Z_MAX_ACCEL 10 //Max acceleration of z axis in mm/s^2
+
+#define BAUDRATE 9600
